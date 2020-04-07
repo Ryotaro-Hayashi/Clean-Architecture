3954711872a98ed882507d80734e7d9c2feb5029
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-07 20:42:00 +0900

    Add：interfacesで使用するメソッドをinfrastructureで定義

--- src/app/infrastructure/sqlhandler.go
+++ src/app/infrastructure/sqlhandler.go
@@ -1,4 +1,4 @@
-// DB接続は、外部パッケージを利用するので内側にルールを持ち込まないようにinfrastructure層に定義
+// ①DB接続は、外部パッケージを利用するので内側にルールを持ち込まないようにinfrastructure層に定義
 
 package infrastructure
 
@@ -8,6 +8,8 @@ import (
   "database/sql"
   // mysql用のドライバー
   _ "github.com/go-sql-driver/mysql"
+  // ②interfacesでDB接続ができるように、interfacesで定義したロジックをインポートして依存関係を逆転させる
+  "app/interfaces/database"
 )
 
 type SqlHandler struct {
@@ -30,4 +32,63 @@ func NewSqlHandler() *SqlHandler {
   sqlHandler := new(SqlHandler)
   sqlHandler.Conn = conn
   return sqlHandler
+}
+
+// interfacesで使うメソッドを定義
+
+// Executeメソッド
+func (handler *SqlHandler) Execute(statement string, args ...interface{}) (database.Result, error) {
+    res := SqlResult{}
+    result, err := handler.Conn.Exec(statement, args...)
+    if err != nil {
+        return res, err
+    }
+    res.Result = result
+    return res, nil
+}
+
+// Queryメソッド
+func (handler *SqlHandler) Query(statement string, args ...interface{}) (database.Row, error) {
+    rows, err := handler.Conn.Query(statement, args...)
+    if err != nil {
+        return new(SqlRow), err
+    }
+    row := new(SqlRow)
+    row.Rows = rows
+    return row, nil
+}
+
+// LastInsertIdメソッドの呼び出しもと
+type SqlResult struct {
+    Result sql.Result
+}
+
+// LastInsertIdメソッド
+func (r SqlResult) LastInsertId() (int64, error) {
+    return r.Result.LastInsertId()
+}
+
+// RowsAffectedメソッド
+func (r SqlResult) RowsAffected() (int64, error) {
+    return r.Result.RowsAffected()
+}
+
+// Scanメソッド, Nextメソッド, Closeメソッドの呼び出しもと
+type SqlRow struct {
+    Rows *sql.Rows
+}
+
+// Scanメソッド
+func (r SqlRow) Scan(dest ...interface{}) error {
+    return r.Rows.Scan(dest...)
+}
+
+// Nextメソッド
+func (r SqlRow) Next() bool {
+    return r.Rows.Next()
+}
+
+// Closeメソッド
+func (r SqlRow) Close() error {
+    return r.Rows.Close()
 }
